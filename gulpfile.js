bc72e120a575d18a2000eb061257c4c493179334
Jonathan Hägglund <jonathanhagglund@live.com>
2023-04-25 14:45:03 +0200

    Make workeable and remove tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,35 @@
 "use strict";
-var gulp = require('gulp');
-var git = require('gulp-git');
-var bump = require('gulp-bump');
-var tag_version = require('gulp-tag-version');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var git = require("gulp-git");
+var bump = require("gulp-bump");
+var tag_version = require("gulp-tag-version");
+var rename = require("gulp-rename");
 
-var uglify = require('gulp-uglify');
-
-var Server = require('karma').Server;
+var uglify = require("gulp-uglify");
 
 function inc(importance) {
-  return gulp.src(['./package.json'])
-    .pipe(bump({type: importance}))
-    .pipe(gulp.dest('./'))
-    .pipe(git.commit('new package version'))
+  return gulp
+    .src(["./package.json"])
+    .pipe(bump({ type: importance }))
+    .pipe(gulp.dest("./"))
+    .pipe(git.commit("new package version"))
     .pipe(tag_version());
 }
 
-gulp.task('patch', function () {
-  return inc('patch');
+gulp.task("patch", function () {
+  return inc("patch");
 });
-gulp.task('feature', function () {
-  return inc('minor');
+gulp.task("feature", function () {
+  return inc("minor");
 });
-gulp.task('release', function () {
-  return inc('major');
-});
-
-/**
- * Run test once and exit
- */
-gulp.task('test', function (done) {
-  new Server({
-    configFile: __dirname + '/karma.conf.js',
-  }, done).start();
+gulp.task("release", function () {
+  return inc("major");
 });
 
-gulp.task('min', function () {
-  return gulp.src(['dist/cytoscape-html-label.js'])
+gulp.task("min", function () {
+  return gulp
+    .src(["dist/cytoscape-html-label.js"])
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist', {ext: '.min.js'}));
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist", { ext: ".min.js" }));
 });
